22a5910322c8cae408914110b562fb6b5ad48567
Eunice_Manan <mananeunice@gmail.com>
2025-04-22 16:00:58 -0400

    Complete Exercise 1 - Workbook 3

--- FamousQuotes/src/main/java/com/pluralsight/App.java
+++ FamousQuotes/src/main/java/com/pluralsight/App.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
         String[] favoriteQuotes = {
                 "No one can make you feel inferior without your consent.",
                 "For our own success to be real, it must contribute to the success of others. ",
@@ -16,21 +18,23 @@ public class App {
                 "Sometimes things have to fall apart to make way for better things. - HIMYM!",
                 "Some day you will be old enough to start reading fairy tales again.",
                 "We come to love not by finding a perfect person, but by learning to see an imperfect peron perfectly.",
-                "Love ia patient, love is kind.",
+                "Love is patient, love is kind."
         };
+        while (true) {
+            try {
+                System.out.println("Select a number between 1 - 10. RIGHT NOW.");
+                int number = input.nextInt();
 
-        Scanner input = new Scanner(System.in);
-
-        try {
-            System.out.println("Select a number between 1 - 10. RIGHT NOW.");
-            int number = input.nextInt();
-            System.out.println(" ");
-            System.out.println("Quote " + number + ": " +favoriteQuotes[number]);
-        } catch (Exception e) {
-            System.out.println("WHAT THE FLIP. THAT ISN'T A NUMBER BETWEEN 1 - 10 >:( !!");
-            throw new RuntimeException(e);
-        }
-        input.close();
+                if (number < 1 || number > 10) {
+                    System.out.println("\nQuote " + number + ": " + favoriteQuotes[number]);
+                    continue;
+            }
+                number --;
+                 System.out.println("\nQuote " + (number+1) + ": " + favoriteQuotes[number]);
+                return;
+            }   catch (Exception e) {
+                    System.out.println("WHAT THE FLIP. THAT ISN'T A NUMBER BETWEEN 1 - 10 >:( !!");
+        }           input.nextLine();
     }
-
+}
 }
